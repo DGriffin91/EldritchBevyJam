11c5fd1f0a886ffa8461d82c21402a197f31005d
DGriffin91 <github@dgdigital.net>
2024-10-04 19:47:18 -0700

    init

---
+++ src/character_controller.rs
@@ -0,0 +1,160 @@
+use bevy::core_pipeline::prepass::{DeferredPrepass, DepthPrepass};
+use bevy::math::vec2;
+use bevy::prelude::*;
+use bevy::window::CursorGrabMode;
+use bevy_fps_controller::controller::{
+    CameraConfig, FpsController, FpsControllerInput, FpsControllerPlugin, LogicalPlayer,
+    RenderPlayer,
+};
+use bevy_rapier3d::prelude::*;
+use bs13::bs13_render::cmaa::Cmaa;
+use bs13::bs13_render::ssao::Ssao;
+use bs13::bs13_render::taa::TaaBundle;
+use bs13::bs13_render::GpuCull;
+use std::f32::consts::TAU;
+
+pub struct CharacterController;
+impl Plugin for CharacterController {
+    fn build(&self, app: &mut App) {
+        app.add_plugins(FpsControllerPlugin)
+            .add_systems(Startup, spawn_player)
+            .add_systems(Update, manage_cursor);
+    }
+}
+
+const SPAWN_POINT: Vec3 = Vec3::new(0.0, 1.625, 0.0);
+
+fn spawn_player(mut commands: Commands, asset_server: Res<AssetServer>) {
+    // Note that we have two entities for the player
+    // One is a "logical" player that handles the physics computation and collision
+    // The other is a "render" player that is what is displayed to the user
+    // This distinction is useful for later on if you want to add multiplayer,
+    // where often time these two ideas are not exactly synced up
+    let height = 3.0;
+    let logical_entity = commands
+        .spawn((
+            //Collider::cylinder(height / 2.0, 0.5),
+            // A capsule can be used but is NOT recommended
+            // If you use it, you have to make sure each segment point is
+            // equidistant from the translation of the player transform
+            Collider::capsule_y(height / 2.0, 0.5),
+            Friction {
+                coefficient: 0.0,
+                combine_rule: CoefficientCombineRule::Min,
+            },
+            Restitution {
+                coefficient: 0.0,
+                combine_rule: CoefficientCombineRule::Min,
+            },
+            ActiveEvents::COLLISION_EVENTS,
+            Velocity::zero(),
+            RigidBody::Dynamic,
+            Sleeping::disabled(),
+            LockedAxes::ROTATION_LOCKED,
+            AdditionalMassProperties::Mass(1.0),
+            GravityScale(0.0),
+            Ccd { enabled: true }, // Prevent clipping when going fast
+            TransformBundle::from_transform(Transform::from_translation(SPAWN_POINT)),
+            LogicalPlayer,
+            FpsControllerInput {
+                pitch: -TAU / 12.0,
+                yaw: TAU * 5.0 / 8.0,
+                ..default()
+            },
+            FpsController {
+                air_acceleration: 150.0,
+                jump_speed: 10.0,
+                ..default()
+            },
+        ))
+        .insert(CameraConfig {
+            height_offset: -0.5,
+        })
+        .id();
+
+    commands.spawn((
+        Camera3dBundle {
+            transform: Transform::from_xyz(0.7, 0.7, 1.0)
+                .looking_at(Vec3::new(0.0, 0.3, 0.0), Vec3::Y),
+            projection: Projection::Perspective(PerspectiveProjection {
+                fov: TAU / 5.0,
+                ..default()
+            }),
+            ..default()
+        },
+        EnvironmentMapLight {
+            diffuse_map: asset_server.load("environment_maps/pisa_diffuse_rgb9e5_zstd.ktx2"),
+            specular_map: asset_server.load("environment_maps/pisa_specular_rgb9e5_zstd.ktx2"),
+            intensity: 1000.0,
+        },
+        Cmaa::default(),
+        TaaBundle::sample4(),
+        DepthPrepass,
+        DeferredPrepass,
+        Ssao,
+        GpuCull {
+            frustum: false,
+            occlusion: false,
+        },
+        RenderPlayer { logical_entity },
+    ));
+}
+
+fn manage_cursor(
+    keys: Res<ButtonInput<KeyCode>>,
+    mut fps_controller: Query<&mut FpsController>,
+    btn: Res<ButtonInput<MouseButton>>,
+    //#[cfg(debug_assertions)] editor_state: Res<EditorState>,
+    mut windows: Query<&mut Window>,
+    //mut contexts: EguiContexts,
+) {
+    //if contexts.ctx_mut().wants_pointer_input() || setting_clock.0 {
+    //    return;
+    //}
+    let mut window = windows.single_mut();
+    let mut fps_controller = fps_controller.single_mut();
+    let cursor_locked = window.cursor.grab_mode == CursorGrabMode::Locked;
+    let mut lock = None;
+    if keys.just_pressed(KeyCode::Tab) {
+        lock = Some(!cursor_locked);
+    }
+    if keys.just_pressed(KeyCode::Escape) || (!cursor_locked && fps_controller.enable_input) {
+        // Unlock
+        lock = Some(false);
+    }
+
+    #[allow(unused_assignments, unused_mut)]
+    let mut editor_active = false;
+
+    //#[cfg(debug_assertions)]
+    //{
+    //    editor_active = editor_state.active;
+    //}
+
+    if btn.just_pressed(MouseButton::Left)
+        && (!fps_controller.enable_input || window.cursor.visible || !cursor_locked)
+        && !editor_active
+    {
+        // Lock
+        lock = Some(true);
+    }
+
+    if let Some(lock) = lock {
+        if lock {
+            // Lock
+            fps_controller.enable_input = true;
+            window.cursor.grab_mode = CursorGrabMode::Locked;
+            window.cursor.visible = false;
+        } else {
+            // Unlock
+            fps_controller.enable_input = false;
+            window.cursor.grab_mode = CursorGrabMode::None;
+            window.cursor.visible = true;
+        }
+    }
+    #[cfg(not(target_os = "macos"))]
+    if cursor_locked {
+        let (w, h) = (window.width(), window.height());
+        window.set_cursor_position(Some(vec2(w / 2.0, h / 2.0)));
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -4,26 +4,111 @@
 // Feel free to delete this line.
 #![allow(clippy::too_many_arguments, clippy::type_complexity)]
 
-use bevy::asset::AssetMetaCheck;
+pub mod character_controller;
+pub mod physics;
+pub mod util;
+
+use std::f32::consts::PI;
+use std::path::PathBuf;
+
+use bevy::diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin};
+use bevy::pbr::CascadeShadowConfigBuilder;
 use bevy::prelude::*;
+use bevy::render::settings::WgpuSettings;
+use bevy::render::RenderPlugin;
+use bevy::window::{PresentMode, WindowResolution};
+use bevy::winit::{UpdateMode, WinitSettings};
+use bevy_mod_mipmap_generator::{generate_mipmaps, MipmapGeneratorPlugin, MipmapGeneratorSettings};
+
+use bs13::bs13_render::dyn_material_blender::AllDynMaterialImagesMaterial;
+
+use bs13::bs13_render::taa::BS13TaaPlugin;
+use bs13::bs13_render::BS13StandardMaterialPluginsSet;
+use bs13_egui::BS13EguiPlugin;
+use character_controller::CharacterController;
+use physics::{AddTrimeshPhysics, PhysicsStuff};
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins.set(AssetPlugin {
-            // Wasm builds will check for meta files (that don't exist) if this isn't set.
-            // This causes errors and even panics in web builds on itch.
-            // See https://github.com/bevyengine/bevy_github_ci_template/issues/48.
-            meta_check: AssetMetaCheck::Never,
+        .insert_resource(AmbientLight::NONE)
+        // TODO include compressed textures?
+        .insert_resource(MipmapGeneratorSettings {
+            anisotropic_filtering: 16,
+            compression: Some(Default::default()),
+            compressed_image_data_cache_path: Some(PathBuf::from("cache")),
+            low_quality: true,
             ..default()
-        }))
+        })
+        .insert_resource(WinitSettings {
+            focused_mode: UpdateMode::Continuous,
+            unfocused_mode: UpdateMode::Continuous,
+        })
+        .add_plugins((
+            DefaultPlugins
+                .set(RenderPlugin {
+                    render_creation: WgpuSettings {
+                        backends: None,
+                        ..default()
+                    }
+                    .into(),
+                    ..default()
+                })
+                .set(WindowPlugin {
+                    primary_window: Some(Window {
+                        // TODO make present mode option?
+                        // TODO full screen?
+                        present_mode: PresentMode::Immediate,
+                        resolution: WindowResolution::new(1920.0, 1080.0)
+                            .with_scale_factor_override(1.0),
+                        ..default()
+                    }),
+                    ..default()
+                }),
+            BS13StandardMaterialPluginsSet,
+            LogDiagnosticsPlugin::default(),
+            FrameTimeDiagnosticsPlugin,
+            MipmapGeneratorPlugin,
+            BS13EguiPlugin,
+            BS13TaaPlugin,
+            PhysicsStuff,
+            CharacterController,
+        ))
         .add_systems(Startup, setup)
+        .add_systems(
+            Update,
+            (
+                generate_mipmaps::<AllDynMaterialImagesMaterial>,
+                //ui_example_system,
+            ),
+        )
         .run();
 }
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
-    commands.spawn(Camera2dBundle::default());
-    commands.spawn(SpriteBundle {
-        texture: asset_server.load("ducky.png"),
-        ..Default::default()
+    commands
+        .spawn(SceneBundle {
+            scene: asset_server.load("temp/init_test_scene.gltf#Scene0"),
+            ..default()
+        })
+        .insert(AddTrimeshPhysics);
+
+    commands.spawn(DirectionalLightBundle {
+        transform: Transform::from_rotation(Quat::from_euler(
+            EulerRot::XYZ,
+            PI * -0.15,
+            PI * 0.13,
+            0.0,
+        )),
+        directional_light: DirectionalLight {
+            shadows_enabled: true,
+            ..default()
+        },
+        cascade_shadow_config: CascadeShadowConfigBuilder {
+            num_cascades: 1,
+            maximum_distance: 40.0,
+            ..default()
+        }
+        .into(),
+        ..default()
     });
 }
